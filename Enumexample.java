0d1203a6ef3ca0bfb174c6ca91c819094b493eb4
pujitha967 <tegadapujitha@gmail.com>
2024-10-04 11:04:43 +0530

    Add files via upload

---
+++ Enumexample.java
@@ -0,0 +1,8 @@
+class Enumexample{
+    public Enum Season{WINTER,SPRING,SUMMER,FALL}
+    //main method
+    public static void main(String[]args){
+        for(Season s:Season values())
+        System.out.println(S);
+    }
+}
